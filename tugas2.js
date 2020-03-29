f20dfb2d216ef2b55d94e0ac6752191409705298
niomicsam <biskuitkelapa@gmail.com>
2020-03-29 12:48:52 +0700

    Tugas 2 JS
    -Buatlah 5 buah variabel dengan 5 tipe data yang berbeda number , string , boolean , null atau undifined dan array
    -Didalam variabel tersebut teman teman isikan nilai yang berbeda tiap variabelnya
    -Setelah itu teman teman tampilkan kelima variabel tersebut dengan console.log()

---
+++ tugas2.js
@@ -0,0 +1,8 @@
+var number = 2;
+var string = "dua"
+var boolean = true;
+var var1 = null;
+var undefined1;
+var array = [0,1,2];
+
+console.log(number +" "+ string +" "+ boolean + " " + var1 + " " + undefined1 + " " + array[2]);
